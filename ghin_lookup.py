339daf3015de7aaae8da0dbc03f38d846ddb997b
billstrand1 <31901296+billstrand1@users.noreply.github.com>
2020-01-02 13:33:58 -0600

    Update ghin_lookup.py

--- ghin_lookup.py
+++ ghin_lookup.py
@@ -36,72 +36,6 @@ def get_index(ghin_no):
 	# print (index_list)
 	player_dict[name_str] = index_int
 
-def compute_handicap_tpc_white(h_i): #for slope rating of 128 TPC whites
-	if h_i >= 0.5 and h_i <= 1.3 : return 1
-	elif h_i >= 1.4 and h_i <= 2.2 : return 2
-	elif h_i >= 2.3 and h_i <= 3.0: return 3
-	elif h_i >= 3.1 and h_i <= 3.9: return 4
-	elif h_i >= 4.0 and h_i <= 4.8 : return 5
-	elif h_i >= 4.9 and h_i <= 5.7 : return 6
-	elif h_i >= 5.8 and h_i <= 6.6 : return 7
-	elif h_i >= 6.7 and h_i <= 7.5 : return 8
-	elif h_i >= 7.6 and h_i <= 8.3 : return 9
-	elif h_i >= 8.4 and h_i <= 9.2 : return 10
-	elif h_i >= 9.3 and h_i <= 10.1 : return 11
-	elif h_i >= 10.2 and h_i <= 11.0 : return 12
-	elif h_i >= 11.1 and h_i <= 11.9 : return 13
-	elif h_i >= 12.0 and h_i <= 12.8 : return 14
-	elif h_i >= 12.9 and h_i <= 13.6 : return 15
-	elif h_i >= 13.7 and h_i <= 14.5 : return 16
-	elif h_i >= 14.6 and h_i <= 15.4 : return 17
-	elif h_i >= 15.5 and h_i <= 16.3 : return 18
-	elif h_i >= 16.4 and h_i <= 17.2 : return 19
-	elif h_i >= 17.3 and h_i <= 18.0 : return 20
-	elif h_i >= 18.1 and h_i <= 18.9 : return 21
-	elif h_i >= 19.0 and h_i <= 19.8 : return 22
-	elif h_i >= 19.9 and h_i <= 20.7 : return 23
-	elif h_i >= 20.8 and h_i <= 21.6 : return 24
-	elif h_i >= 21.7 and h_i <= 22.5 : return 25
-	elif h_i >= 22.6 and h_i <= 23.3 : return 26
-	elif h_i >= 23.4 and h_i <= 24.2 : return 27
-	elif h_i >= 24.3 and h_i <= 25.1 : return 28
-	elif h_i >= 25.2 and h_i <= 26.0 : return 29
-	elif h_i >= 26.1 and h_i <= 26.9 : return 30
-	else: return 0
-
-def compute_handicap_cwv_white(h_i): #for slope rating of 130 CWV whites
-	if h_i >= 0.5 and h_i <= 1.3 : return 1
-	elif h_i >= 1.4 and h_i <= 2.1 : return 2
-	elif h_i >= 2.2 and h_i <= 3.0: return 3
-	elif h_i >= 3.1 and h_i <= 3.9: return 4
-	elif h_i >= 4.0 and h_i <= 4.7 : return 5
-	elif h_i >= 4.8 and h_i <= 5.6 : return 6
-	elif h_i >= 5.7 and h_i <= 6.5 : return 7
-	elif h_i >= 6.6 and h_i <= 7.3 : return 8
-	elif h_i >= 7.4 and h_i <= 8.2 : return 9
-	elif h_i >= 8.3 and h_i <= 9.1 : return 10
-	elif h_i >= 9.2 and h_i <= 9.9 : return 11
-	elif h_i >= 10.0 and h_i <= 10.8 : return 12
-	elif h_i >= 10.9 and h_i <= 11.7 : return 13
-	elif h_i >= 11.8 and h_i <= 12.6 : return 14
-	elif h_i >= 12.7 and h_i <= 13.4 : return 15
-	elif h_i >= 13.5 and h_i <= 14.3 : return 16
-	elif h_i >= 14.4 and h_i <= 15.2 : return 17
-	elif h_i >= 15.3 and h_i <= 16.0 : return 18
-	elif h_i >= 16.1 and h_i <= 16.9 : return 19
-	elif h_i >= 17.0 and h_i <= 17.8 : return 20
-	elif h_i >= 17.9 and h_i <= 18.6 : return 21
-	elif h_i >= 18.7 and h_i <= 19.5 : return 22
-	elif h_i >= 19.6 and h_i <= 20.4 : return 23
-	elif h_i >= 20.5 and h_i <= 21.2 : return 24
-	elif h_i >= 21.3 and h_i <= 22.1 : return 25
-	elif h_i >= 22.2 and h_i <= 23.0 : return 26
-	elif h_i >= 23.1 and h_i <= 23.9 : return 27
-	elif h_i >= 24.0 and h_i <= 24.7 : return 28
-	elif h_i >= 24.8 and h_i <= 25.6 : return 29
-	elif h_i >= 25.7 and h_i <= 26.5 : return 30
-	else: return 0
-
 def get_index_from_ghin():
 	print('get_index_from_ghin entered')
 
@@ -127,6 +61,8 @@ def get_index_from_prefilled_dict():  #pre-defined for testing only
 #--------------------START LIST, DICT DEFINITINS-----------------------------
 index_list = []
 player_dict = {} # list of players : h_i from either prefilled or ghin
+tpc_slope_white = 128
+cwv_slope_white= 130 
 
 name_dict = {'Mr. Jack Carroll': 'Jack', 'Bob Heard': 'Bob', 'Larry Traub': 'Larry', 'Jim Sido': 'Jim S.', 
 'Curt Fitzgerald': 'Curt', 'Jimmy Wickham': 'Jimmy', 'Dr. Richard Humphrey': 'Hump', 'Kent Fannon': 'Kent', 
@@ -141,8 +77,8 @@ ghin = ['3660603','4548474','4787165','2379581','3661053','3660992','3660486','5
 #--------------------START CODE EXECUTION-----------------------------
 
 print('Program starts')
-# get_index_from_ghin() #for production, player_dict is populated with a new ghin download
-player_dict = get_index_from_prefilled_dict() #for testing only, player_dict is predefined from a previous ghin download
+get_index_from_ghin() #for production, player_dict is populated with a new ghin download
+# player_dict = get_index_from_prefilled_dict() #for testing only, player_dict is predefined from a previous ghin download
 
 tpc_list = []  #accumulate list of handicaps for sorting purposes
 cwv_list = []  #accumulate list of handicaps for sorting purposes
@@ -152,9 +88,9 @@ for player in player_dict:
 	h_i = float(player_dict[player])
 	name = player
 	# print (h_i)
-	handicap_tpc = compute_handicap_tpc_white(h_i)
+	handicap_tpc = round (h_i * tpc_slope_white / 113)
 	tpc_list.append(handicap_tpc)
-	handicap_cwv = compute_handicap_cwv_white(h_i)
+	handicap_cwv = round (h_i * cwv_slope_white / 113)
 	cwv_list.append(handicap_cwv)
 	# print(f"Name: {name} Index: {h_i}, TPC: {handicap_tpc}, CWV: {handicap_cwv}")
 
@@ -170,4 +106,21 @@ for player in player_dict:
 	cwv_handicap = compute_handicap_cwv_white(float(player_dict[player]))
 	print(f" {name_dict[player]} : {player_dict[player]}, TPC: {tpc_handicap} ({tpc_handicap-tpc_min}), CWV: {cwv_handicap} ({cwv_handicap-cwv_min})")
 	
-	
+
+# Bob : 6.2, TPC: 7 (0), CWV: 7 (0)
+#  Larry : 8.4, TPC: 10 (3), CWV: 10 (3)
+#  Jim S. : 8.2, TPC: 9 (2), CWV: 9 (2)
+#  Curt : 17.5, TPC: 20 (13), CWV: 20 (13)
+#  Jimmy : 13.9, TPC: 16 (9), CWV: 16 (9)
+#  Hump : 16.9, TPC: 19 (12), CWV: 19 (12)
+#  Kent : 12.5, TPC: 14 (7), CWV: 14 (7)
+#  Bill B. : 15.6, TPC: 18 (11), CWV: 18 (11)
+#  Doug : 19.1, TPC: 22 (15), CWV: 22 (15)
+#  Rocky : 16.1, TPC: 18 (11), CWV: 19 (12)
+#  Will : 12.7, TPC: 14 (7), CWV: 15 (8)
+#  Reid : 11.2, TPC: 13 (6), CWV: 13 (6)
+#  Jack : 10.1, TPC: 11 (4), CWV: 12 (5)
+#  Al : 14.5, TPC: 16 (9), CWV: 17 (10)
+#  Dan : 13.0, TPC: 15 (8), CWV: 15 (8)
+#  Bill S. : 11.3, TPC: 13 (6), CWV: 13 (6)
+# https://widgets.ghin.com/HandicapLookupResults.aspx?entry=1&dynamic=&small=1&css=CGA03&ghinno=0053161&hidename=0&showmsg=0&showheader=1&showtabheader=0&combinehieff=0&showheadertext=1&showfootertext=1&tab=0'
